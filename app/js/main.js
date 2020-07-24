75c62ffe9ab60e67cf5d6ea618a649a2be95c327
ALeksiuslearn <igotgoldenroshan@gmail.com>
2020-07-24 06:21:27 +0300

    added full adaptive, course cleared

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,44 @@ $(function () {
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 501,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+    ],
   });
   $(".js-range-slider").ionRangeSlider({
     type: "double",
